b5f19a71b39a012ec8edf677fbae53c9343aca49
KalmonV <kalmonvaladao@gmail.com>
2025-05-17 11:33:14 -0300

    Fix

--- test.js
+++
@@ -1,9 +0,0 @@
-import { danfe } from './dist/index.js';
-import fs from "fs";
-
-let xml = fs.readFileSync("./exemplos/NFe.xml", { encoding: "utf8" });
-console.log(xml)
-let pdf = new danfe({ xml: xml });
-pdf.getPDF().then(res => {
-    fs.writeFileSync("./exemplos/NFe.pdf", Buffer.from(res, 'base64'));
-})
